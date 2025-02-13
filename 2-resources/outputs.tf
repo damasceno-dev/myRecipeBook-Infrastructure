171fcd76b26d1f5f79e07f0aa80551579dc38533
fernandodamasceno <damasceno.developer21@gmail.com>
2025-02-13 11:35:14 -0300

    Refactor outputs and mask sensitive variables in CI.
    Renamed ECR output variables for clarity and updated their references in the deployment workflow. Added masking for sensitive DB password in GitHub Actions logs and improved comments for better maintainability.

--- 2-resources/outputs.tf
+++ 2-resources/outputs.tf
@@ -1,9 +1,9 @@
-output "repository_arn" {
+output "ecr_repository_arn" {
   value       = module.ecr.repository_arn
   description = "ARN of the ECR repository"
 }
 
-output "repository_url" {
+output "ecr_repository_url" {
   value       = module.ecr.repository_url
   description = "URL of the ECR repository"
 }
