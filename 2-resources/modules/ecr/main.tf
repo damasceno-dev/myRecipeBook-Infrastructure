184862155e801f2d7d6ffddc5184ca0fbbccb25b
fernandodamasceno <damasceno.developer21@gmail.com>
2025-02-16 19:38:25 -0300

    Update ECR policy to refine permissions and include App Runner
    Added policies to allow App Runner and its IAM role to pull images and described repositories. Included a mandatory policy to allow all principals to obtain ECR authorization tokens. This ensures proper access control and functionality alignment with service requirements.

--- 2-resources/modules/ecr/main.tf
+++ 2-resources/modules/ecr/main.tf
@@ -13,30 +13,61 @@ resource "aws_ecr_repository" "ecr" {
     IAC  = "True"
   }
 }
+
 resource "aws_ecr_repository_policy" "app_runner_ecr_policy" {
   repository = aws_ecr_repository.ecr.name
 
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
+      # 1️⃣ Allow App Runner to pull images from ECR
       {
-        Sid    = "AllowAppRunnerToPull",
+        Sid    = "AllowAppRunnerPull",
         Effect = "Allow",
         Principal = {
-          Service = "apprunner.amazonaws.com",
-          AWS     = "arn:aws:iam::${var.account_id}:role/${var.prefix}-AppRunnerServiceRole"
+          Service = "apprunner.amazonaws.com"
         },
         Action = [
           "ecr:GetDownloadUrlForLayer",
           "ecr:BatchGetImage",
           "ecr:GetAuthorizationToken",
-          "ecr:ListImages"
+          "ecr:ListImages",
+          "ecr:DescribeRepositories"
         ],
         Resource = aws_ecr_repository.ecr.arn
+      },
+
+      # 2️⃣ Allow the IAM Role to pull images from ECR
+      {
+        Sid    = "AllowAppRunnerRolePull",
+        Effect = "Allow",
+        Principal = {
+          AWS = "arn:aws:iam::${var.account_id}:role/${var.prefix}-AppRunnerServiceRole"
+        },
+        Action = [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:GetAuthorizationToken",
+          "ecr:ListImages",
+          "ecr:DescribeRepositories"
+        ],
+        Resource = aws_ecr_repository.ecr.arn
+      },
+
+      # 3️⃣ Allow all IAM users and services to get Authorization Token (Mandatory)
+      {
+        Sid    = "AllowECRLogin",
+        Effect = "Allow",
+        Principal = "*",
+        Action = [
+          "ecr:GetAuthorizationToken"
+        ],
+        Resource = "*"
       }
     ]
   })
 }
+
 resource "aws_ecr_lifecycle_policy" "ecr_policy" {
   repository = aws_ecr_repository.ecr.name
 
